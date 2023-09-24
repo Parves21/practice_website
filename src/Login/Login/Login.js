18b73bea620801de002db3a23dab17ec0fb060c7
Parves21 <nurislam42021@gmail.com>
2023-09-24 15:28:25 -0700

    React Font Add

--- src/Login/Login/Login.js
+++ src/Login/Login/Login.js
@@ -4,7 +4,7 @@ import * as React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import './Login.css';
-import { FaUserCircle, FaEnvelope } from "react-icons/fa";
+import { FaUserCircle, FaEnvelope, FaLock } from "react-icons/fa";
 import { Link } from 'react-router-dom';
 
 function Login() {
@@ -14,16 +14,16 @@ function Login() {
              <div className='d-flex justify-content-center align-items-center'>
                 <Form id='LoginPage' className='w-50 mt-4'>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label><h3>Email</h3></Form.Label>
+                        <Form.Label><p className='mb-1'> <FaEnvelope/> Email</p></Form.Label>
                         <Form.Control type="email" placeholder="Enter email" required />
                         <Form.Text className="text-muted">
                             <p className='text-success'>We'll never share your Email.</p>
                         </Form.Text>
                     </Form.Group>
                     <Form.Group className="mb-1" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
+                        <Form.Label> <FaLock/> Password</Form.Label>
                         <Form.Control type="password" placeholder="Password" required />
-                        <div className='d-flex justify-content-around'>
+                        <div className='mt-3 d-flex justify-content-around'>
                             <p className='text-success'>Don't have an Account !!</p>
                             <Link className='text-danger' to='/register'><small>SignUp</small></Link>
                         </div>
